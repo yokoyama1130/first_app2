f9cd6ccbe628e03159745652ee77ea156b6cc876
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-01 13:29:07 +0000

    first_app2-3

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,8 @@
+class HomesController < ApplicationController
+  # アクションを定義する
+  # アクションはコントローラに定義されている処理内容
+  # つまり、この中にルーティングがアクセスした時にどのように割り振るのか決める
+  def top
+    # この中にtopにアクセスが来た時にどのように動くのか決める
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 
+  # /topって検索したら、homesコントローラの中のtopアクションが呼び出されるという意味
+  get 'top' => 'homes#top'
 end
